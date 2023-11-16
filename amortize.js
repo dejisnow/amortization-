fa033efae9a403997409d252d2ec8e4cb4a40aea
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2023-11-16 19:37:27 +0100

    Update amortize.js

--- amortize.js
+++ amortize.js
@@ -1,8 +1,9 @@
 window.onload=()=>{
-let parentRow = document.querySelector(".parentRow")
+let parent = document.querySelector(".parent")
     
 let main = document.querySelector(".main")
 let amtB= document.querySelector(".amtB")
+let intP= document.querySelector(".intP")
 let intOnAmount= document.querySelector(".interest")
 let amtE= document.querySelector(".amtE")
 
@@ -10,8 +11,6 @@ let inputP = document.getElementById("inputP")
 let inputI = document.getElementById("inputI")
 let inputY = document.getElementById("inputY")
 let submit = document.getElementById("submit")
-
-
 
 
 
@@ -29,45 +28,7 @@ let intr =   inputI.value ;
 let years = inputY.value ;
 //Arrays
 let amtBArray= [];
-let intArray= [];
-let annArray = [];
-let amtEndArray = [];
-
-
-
-let interestCalculation = prin * intr
-    let interest = intr /100
-window.onload=()=>{
-let parentRow = document.querySelector(".parentRow")
-    
-let main = document.querySelector(".main")
-let amtB= document.querySelector(".amtB")
-let intOnAmount= document.querySelector(".interest")
-let amtE= document.querySelector(".amtE")
-
-let inputP = document.getElementById("inputP")
-let inputI = document.getElementById("inputI")
-let inputY = document.getElementById("inputY")
-let submit = document.getElementById("submit")
-
-
-
-
-
-submit.addEventListener("click",(e)=>{
-    e.preventDefault()
-    
-    return console.log(annuity())
-}
- )
- 
- 
- function annuity(){
-let prin = inputP.value ;
-let intr =   inputI.value ;
-let years = inputY.value ;
-//Arrays
-let amtBArray= [];
+let intPArray = [];
 let intArray= [];
 let annArray = [];
 let amtEndArray = [];
@@ -81,126 +42,39 @@ let interestCalculation = prin * intr
    
    let result = prin / division
 
-  for(let i = prin; i >=0; i-=result.toFixed(2)){ 
-   let intt= ( i * interest)
-  let addIntt = intt + i
+  for(let i = parseInt(prin); i >=0; i-=result.toFixed(2)){ 
+  
+   let intt= i * interest 
+  
+  let addIntt = i + intt 
+  
  let endAmnt = addIntt - result 
 
+ amtBArray.push(Math.ceil(i))
+ intArray.push(Math.ceil(intt))
+ intPArray.push(Math.ceil(addIntt))
+ annArray.push(Math.ceil(result))
+ amtEndArray.push(Math.ceil(endAmnt))
  
- 
- 
- 
-  amtB.innerHTML += ` <li>${i}</li> `
-   intOnAmount.innerHTML +=`<li>${addIntt}</li> `
-    
-  main.innerHTML +=`<li> ${result.toFixed(2)}</li>`
-  
-  amtE.innerHTML +=` <li>${endAmnt.toFixed(2)} </li>`
 
  }
+ amtBArray.map(x=> (amtB.innerHTML += ` <li>${x}</li> `))
+ 
+ intArray.map(x=> (intOnAmount.innerHTML += ` <li>${x}</li> `))
+ 
+ intPArray.map(x=> (intP.innerHTML += ` <li>${x}</li> `))
  
  
- 
- }
- 
- /*
-
-let principal = 1000000
-let interest = 19
-let years = 5
-
-
- let res = annuity(inputValue,interest,years)
- 
- 
-
-
-
-function cnt(pri, am){
-let count = 0;
-for(let i = pri; i >=0; i-=am){ 
-amtB.innerHTML +=`
-  100000
-  `
-    
-  main.innerHTML +=`
+ annArray.map(x=> (main.innerHTML += ` <li>${x}</li> `))
   
-  ${i.toFixed(1)}
-  
-  
-  
-  `
-    
-    }
-   
-  return count
-      
- }
- cnt(principal,res)
-
-
-
-
-
-A = P i(1+i)n/(1+i)n - 1
-
-P = A = 1-(1+i)-5/i
-
-
-
-
-
-*/
-
-
-
-}￼Enter   let num=  1-(1+interest)**-years
-   let division= num/interest
-   
-   let result = prin / division
-
-  for(let i = prin; i >=0; i-=result.toFixed(2)){ 
-   let intt= ( i * interest)
-  let addIntt = intt + i
- let endAmnt = addIntt - result 
-
- 
- 
- 
- 
-  amtB.innerHTML += ` <li>${i}</li> `
-   intOnAmount.innerHTML +=`<li>${addIntt}</li> `
-    
-  main.innerHTML +=`<li> ${result.toFixed(2)}</li>`
-  
-  amtE.innerHTML +=` <li>${endAmnt.toFixed(2)} </li>`
-
- }
+ amtEndArray.map(x=> (amtE.innerHTML += ` <li>${x}</li> `))
  
  
  
  }
-
-let principal = 1000000
-let interest = 19
-let years = 5
-
-
- let res = annuity(inputValue,interest,years)
  
  
 
 
 
-function cnt(pri, am){
-let count = 0;
-for(let i = pri; i >=0; i-=am){ 
-amtB.innerHTML +=`
-  100000
-  `
-    
-  main.innerHTML +=`
-  
-  ${i.toFixed(1)}
-  
-  
+}
